dc84239720f15d6e24dea6da0eea4e1ff22bf13c
Janto Andriano <motulojanto@gmail.com>
2021-02-17 21:58:42 +0800

    fix some function

--- users/user.service.js
+++ users/user.service.js
@@ -5,11 +5,7 @@ const db = require('_helpers/db');
 
 module.exports = {
     authenticate,
-    getAll,
-    getById,
     create,
-    update,
-    delete: _delete
 };
 
 async function authenticate({ username, password }) {
@@ -23,13 +19,6 @@ async function authenticate({ username, password }) {
     return { ...omitHash(user.get()), token };
 }
 
-async function getAll() {
-    return await db.User.findAll();
-}
-
-async function getById(id) {
-    return await getUser(id);
-}
 
 async function create(params) {
     // validate
@@ -46,40 +35,9 @@ async function create(params) {
     await db.User.create(params);
 }
 
-async function update(id, params) {
-    const user = await getUser(id);
 
-    // validate
-    const usernameChanged = params.username && user.username !== params.username;
-    if (usernameChanged && await db.User.findOne({ where: { username: params.username } })) {
-        throw 'Username "' + params.username + '" is already taken';
-    }
-
-    // hash password if it was entered
-    if (params.password) {
-        params.hash = await bcrypt.hash(params.password, 10);
-    }
-
-    // copy params to user and save
-    Object.assign(user, params);
-    await user.save();
-
-    return omitHash(user.get());
-}
-
-async function _delete(id) {
-    const user = await getUser(id);
-    await user.destroy();
-}
 
 // helper functions
-
-async function getUser(id) {
-    const user = await db.User.findByPk(id);
-    if (!user) throw 'User not found';
-    return user;
-}
-
 function omitHash(user) {
     const { hash, ...userWithoutHash } = user;
     return userWithoutHash;
